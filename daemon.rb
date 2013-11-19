e5dff01f36c551e8e857e31cfa68003a0eb51b6c
Kieran Manning <kieran.manning@wonga.com>
2013-11-19 11:39:13 +0000

    typo

--- daemon.rb
+++ daemon.rb
@@ -5,4 +5,4 @@ require_relative 'pantry_ad_domain_left_event_handler/pantry_ad_domain_left_even
 
 config_name = File.join(File.dirname(File.symlink?(__FILE__) ? File.readlink(__FILE__) : __FILE__), "config", "daemon.yml")
 Wonga::Daemon.load_config(File.expand_path(config_name))
-Wonga::Daemon.run(Wonga::Daemon::PantryAdDomainLeftEventHandler.new(Wonga::Daemon.api_client, Wonga::Daemon.logger))
+Wonga::Daemon.run(Wonga::Daemon::PantryAdDomainLeftEventHandler.new(Wonga::Daemon.pantry_api_client, Wonga::Daemon.logger))
