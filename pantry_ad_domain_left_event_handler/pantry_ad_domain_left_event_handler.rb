671090b71de0cee280117d9ec50ee55caaf3530d
Kieran Manning <kieran.manning@wonga.com>
2013-11-20 16:36:35 +0000

    setting api call back params

--- pantry_ad_domain_left_event_handler/pantry_ad_domain_left_event_handler.rb
+++ pantry_ad_domain_left_event_handler/pantry_ad_domain_left_event_handler.rb
@@ -8,8 +8,7 @@ module Wonga
 
       def handle_message(message)
         @logger.info "Updating domain left status for Request:#{message['pantry_request_id']}, Name:#{message['instance_name']}, InstanceID:#{message['instance_id']}"
-        #TO DO
-        #@api_client.update_ec2_instance(message['pantry_request_id'], { joined: true, instance_id: message['instance_id'] })
+        @api_client.update_ec2_instance(message['pantry_request_id'], { joined: false })
         @logger.info "Updating domain left status for Request:#{message['pantry_request_id']} succeeded"
       end
     end
