7da42271576a1297fc036deba3b7693b29bcff42
Riccardo Tacconi <Riccardo.Tacconi@wonga.com>
2014-01-10 12:25:46 +0000

    Modified message

--- pantry_ad_domain_left_event_handler/pantry_ad_domain_left_event_handler.rb
+++ pantry_ad_domain_left_event_handler/pantry_ad_domain_left_event_handler.rb
@@ -8,13 +8,9 @@ module Wonga
 
       def handle_message(message)
         @logger.info "Updating domain left status for Request:#{message['id']}, Name:#{message['instance_name']}, InstanceID:#{message['instance_id']}"
-        @api_client.send_put_request(
-          "/api/ec2_instances/#{message['id']}",
-          {
-            event: :terminated,
-            joined: :false
-          }
-        )
+        message[:event] = :terminated
+        message[:joined] = :false
+        @api_client.send_put_request("/api/ec2_instances/#{message['id']}", message)
         @logger.info "Updating domain left status for Request:#{message['id']} succeeded"
       end
     end
