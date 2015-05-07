2a9414aad3be9f64afe6316fbaa191004d3548b3
Alex Slynko <alex.slynko@wonga.com>
2015-05-07 11:50:25 +0100

    RM-1764 Update gems, added rubocop
    * Added .rubocop.yml
    
    Change-Id: Ife3f7d26011c44e9e11d873400216ceb629a3861

--- spec/pantry_ad_domain_left_event_handler/pantry_ad_domain_left_event_handler_spec.rb
+++ spec/pantry_ad_domain_left_event_handler/pantry_ad_domain_left_event_handler_spec.rb
@@ -1,17 +1,18 @@
 require 'spec_helper'
+require 'logger'
 require_relative '../../pantry_ad_domain_left_event_handler/pantry_ad_domain_left_event_handler'
 
-describe Wonga::Daemon::PantryAdDomainLeftEventHandler do
-  let(:config) {
+RSpec.describe Wonga::Daemon::PantryAdDomainLeftEventHandler do
+  let(:config) do
     {
-      "pantry" => {
-        "api_key" => "some_api_key",
-        "request_timeout" => 10,
-        "url" => "http://some.url"
+      'pantry' => {
+        'api_key' => 'some_api_key',
+        'request_timeout' => 10,
+        'url' => 'http://some.url'
       }
     }
-  }
-  let(:logger) { instance_double('Logger').as_null_object }
+  end
+  let(:logger) { instance_double(Logger).as_null_object }
 
   subject { Wonga::Daemon::PantryAdDomainLeftEventHandler.new(config, logger) }
 
